d9a89f7ab2c13ec0c108cc472f36d53c11f54404
C-O-D-Y <saurabh.chauhan@atmecs.com>
2019-09-19 20:05:53 +0530

    1st Commit with framework for testing and executable jar

---
+++ framework/src/main/java/com/atmecs/constants/FilePath.java
@@ -0,0 +1,31 @@
+package com.atmecs.constants;
+
+import java.io.File;
+
+/**
+ * In this Class, All the file paths are saved
+ */
+public class FilePath {
+
+
+	public final static String USER_HOME = System.getProperty("user.dir") + File.separator;
+	public final static String RESOURCES_HOME = USER_HOME + "src" + File.separator + "test" + File.separator
+			+ "resources" + File.separator;
+
+	public final static String LIB_HOME = USER_HOME + "lib" + File.separator;
+
+	
+	public final static String CHROME_PATH = LIB_HOME + "chromedriver.exe";
+	public final static String FIREFOX_PATH = LIB_HOME + "geckodriver.exe";
+	public final static String IE_PATH = LIB_HOME + "IEDriver.exe";
+
+	public static final String EXTENT_REPORT_FILE = USER_HOME + "test-output" + File.separator + "ExtentReport.html";
+	public static final String EXTENT_CONFIG = USER_HOME + "extent-config.xml";
+	public static final String FAILED_SCREENSHOT_FILE = USER_HOME + "FailedTestScreenShots" + File.separator;
+	public final static String LOG4J_FILE = RESOURCES_HOME + "log4j" + File.separator + "log4j.properties";
+	public final static int TIMEOUT_INSECONDS = 30;
+
+	public static void main(String[] args) {
+		System.out.println(EXTENT_CONFIG);
+	}
+}
